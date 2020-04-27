19c41705c1565d73029e9c1b59b11393d57e90d2
Marcus Lau <lau.marcus76@gmail.com>
2020-04-27 20:13:56 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+  
+  # bind instance variables with @ to an instance of a class
+  
+  def name=(dog_name)
+    @this_dogs_name = dog_name
+  end
+  
+  def name
+    @this_dogs_name #  typical scope issue
+  end
+  
+end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+ 
+puts lassie.name
+ 
